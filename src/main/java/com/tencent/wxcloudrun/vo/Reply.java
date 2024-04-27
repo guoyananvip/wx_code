3e3edd296a7306ee411755a61fa8cbc0bf7832a5
win <guoyananchn@163.com>
2024-04-28 00:37:17 +0800

    正文

---
+++ src/main/java/com/tencent/wxcloudrun/vo/Reply.java
@@ -0,0 +1,17 @@
+package com.tencent.wxcloudrun.vo;
+
+/**
+ * @author: 王富贵
+ * @description:
+ * @createTime: 2024年04月27日 01:06:17
+ */
+public class Reply {
+    private String ToUserName;
+    private String FromUserName;
+
+    private Long CreateTime;
+
+    private String MsgType;
+
+    private String Content;
+}
